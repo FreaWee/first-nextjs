343928cc566dea2158f892649cddd99c7d00364c
freawee <abdouobaid@hotmail.com>
2024-06-04 22:58:18 +0200

    Buy page

---
+++ src/app/Buy/page.js
@@ -0,0 +1,58 @@
+export default function () {
+  return (
+    <>
+      <div class="w-3/4 m-auto px-2 h-16 bg-green-300 flex justify-between items-center">
+        <div>WA</div>
+        <div>
+          <img src="/cart.png" class="w-10 h-10"></img>
+        </div>
+      </div>
+      <div class="w-3/4 mx-auto mt-20 grid lg:grid-cols-3 gap-4 text-center">
+        <div class="border rounded-lg p-4">
+          <img class="object-fill w-full h-40 mb-4" src="/bottle.jpg"></img>
+          This is the first bottle
+          <div>
+            <button class="bg-blue-300 rounded-md p-3 px-8 my-4">BUY</button>
+          </div>
+        </div>
+        <div class="border rounded-lg p-4">
+          <img class="object-fill w-full h-40 mb-4" src="/bottle.jpg"></img>This
+          is the second bottle
+          <div>
+            <button class="bg-blue-300 rounded-md p-3 px-8 my-4">BUY</button>
+          </div>
+        </div>
+        <div class="border rounded-lg p-4">
+          <img class="object-fill w-full h-40 mb-4" src="/bottle.jpg"></img>This
+          is the third bottle
+          <div>
+            <button class="bg-blue-300 rounded-md p-3 px-8 my-4">BUY</button>
+          </div>
+        </div>
+      </div>
+      <div class="w-3/4 mx-auto mt-20 grid lg:grid-cols-3 gap-4 text-center">
+        <div class="border rounded-lg p-4">
+          <img class="object-fill w-full h-40 mb-4" src="/bottle.jpg"></img>
+          This is the first bottle
+          <div>
+            <button class="bg-blue-300 rounded-md p-3 px-8 my-4">BUY</button>
+          </div>
+        </div>
+        <div class="border rounded-lg p-4">
+          <img class="object-fill w-full h-40 mb-4" src="/bottle.jpg"></img>This
+          is the second bottle
+          <div>
+            <button class="bg-blue-300 rounded-md p-3 px-8 my-4">BUY</button>
+          </div>
+        </div>
+        <div class="border rounded-lg p-4">
+          <img class="object-fill w-full h-40 mb-4" src="/bottle.jpg"></img>This
+          is the third bottle
+          <div>
+            <button class="bg-blue-300 rounded-md p-3 px-8 my-4">BUY</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
